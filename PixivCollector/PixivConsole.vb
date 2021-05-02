d87013b88137d0f428050094e80df99d8c75e384
ApolloEddy <62823452+ApolloEddy@users.noreply.github.com>
2021-05-02 17:47:01 +0800

    啊啊啊为什么推送不上来啊QwQ

--- PixivCollector/PixivConsole.vb
+++ PixivCollector/PixivConsole.vb
@@ -1,6 +1,8 @@
 ﻿Module PixivConsole
 
 	Sub Main(arg As String())
+
+		' 以下为另一个测试单元
 		Console.Title = My.Application.Info.Title
 		Dim stopwatch As New Stopwatch()
 		stopwatch.Start()
@@ -8,9 +10,13 @@
 			Dim p As New PixivRank()
 
 		End If
+
+		' 以下为测试单元
 		Test(arg(0))
 
 		Console.WriteLine($"finish!  用时：{Format(stopwatch.ElapsedMilliseconds / 1000, "0.000")}s")
+
+		' 以下为结束时控制代码
 		Console.Title = My.Application.Info.Title + “完成”
 		stopwatch.Stop()
 		Console.ReadKey()
